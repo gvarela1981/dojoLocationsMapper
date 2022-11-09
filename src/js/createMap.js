f0b6aa436b4ed08a0b80760c875bcc0097a3df4d
Gonzalo Varela <gonzalo.varela1981@gmail.com>
2022-11-09 17:17:01 -0300

    Add function to add geoJson POI to map
    Add functionality to add a categorized POI with 3 styles and a default

--- src/js/createMap.js
+++ src/js/createMap.js
@@ -15,27 +15,53 @@ function createMap() {
     // add a marker in the given location
     //L.marker(center).addTo(map);
 
-    let testCoords = {
-        "type": "Feature",
-        "properties": {"categoria": "Residencial"},
-        "geometry": {
-            "type": "Point",
-            "coordinates": [-58.3816, -34.60378]
-        }
-    }
+    addPOI(map, testCoords)
+}
 
-    L.geoJSON(testCoords, {
-        pointToLayer(feature, latlng) {
-			return L.circleMarker(latlng, {
-				radius: 8,
-				fillColor: '#ff7800',
-				color: '#000',
-				weight: 1,
-				opacity: 1,
-				fillOpacity: 0.8
-			});
-		}
-    }).addTo(map);
-    console.log("coord added")
- 
+let residentialStyle = {
+  radius: 8,
+  fillColor: '#00f',
+  color: '#000',
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+}
+
+let comercialStyle = Object.assign({}, residentialStyle) 
+let mixtaStyle = Object.assign({}, residentialStyle) 
+let defaultStyle = Object.assign({}, residentialStyle) 
+
+comercialStyle["fillColor"] = '#0f0'
+mixtaStyle["fillColor"] = '#f00'
+defaultStyle["fillColor"] = '#afafaf'
+
+let testCoords = {
+  "type": "Feature",
+  "properties": {"categoria": "Residencial"},
+  "geometry": {
+      "type": "Point",
+      "coordinates": [-58.3816, -34.60378]
+  }
+}
+
+function addPOI(map, feature) {
+  let currentStyle;
+  L.geoJSON(feature, {
+    pointToLayer(feature, latlng) {
+      if (feature.properties.categoria) {
+        switch (feature.properties.categoria) {
+          case 'Residencial':
+            currentStyle = Object.assign({}, residentialStyle);
+            break;
+          case 'Comercial':
+            currentStyle = Object.assign({}, comercialStyle);
+            break;
+          default:
+            currentStyle = Object.assign ({}, defaultStyle);
+            break;
+        }
+      }
+      return L.circleMarker(latlng, currentStyle);
+    }
+  }).addTo(map);
 }
